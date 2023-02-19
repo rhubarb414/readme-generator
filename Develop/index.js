ddd384e8130813471ceb0d208821ee5ceee60da0
rhubarb414 <ctophr.dubois@gmail.com>
2023-02-18 19:03:22 -0800

    adds questions array

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,17 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "Enter the title of your project",
+  "Enter the description",
+  "Add a table of contents",
+  "Enter info about insallation",
+  "Enter info about usage",
+  "Enter license info",
+  "Add contributors",
+  "Enter test info",
+  "Add contact info for questions",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
